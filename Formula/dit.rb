81d5517451fb13ae06cb405ccacec92dab69d558
Jake Barnby <jakeb994@gmail.com>
2022-04-15 09:42:15 +1200

    Make docker optional

--- Formula/dit.rb
+++ Formula/dit.rb
@@ -5,8 +5,8 @@ class Dit < Formula
   sha256 "3559fdfbd07b36f351eb692f2651e70e369f6c0d29b1b39e301318013aac5122"
   license "MIT"
 
-  depends_on "docker"
-  depends_on "docker-compose"
+  depends_on "docker" => :optional
+  depends_on "docker-compose" => :optional
   depends_on "lazydocker" => :optional
 
   def install
