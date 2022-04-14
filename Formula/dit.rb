8d9ba999f73087fe357c819aba32dd039436e566
Jake Barnby <jakeb994@gmail.com>
2022-04-14 23:24:59 +1200

    Add dit formula

---
+++ Formula/dit.rb
@@ -0,0 +1,19 @@
+class Dit < Formula
+  desc "Developer Integrated Toolkit CLI"
+  homepage "https://github.com/abnegate/dit"
+  url "https://github.com/abnegate/dit/archive/refs/tags/0.1.0.tar.gz"
+  sha256 "3559fdfbd07b36f351eb692f2651e70e369f6c0d29b1b39e301318013aac5122"
+  license "MIT"
+
+  depends_on "docker"
+  depends_on "docker-compose"
+  depends_on "lazydocker" => :optional
+
+  def install
+    bin.install "dit"
+  end
+
+  test do
+    system "false"
+  end
+end
