45e4288cae16d0220d9fc0e439be583dfc4be49d
Jake Barnby <jakeb994@gmail.com>
2022-08-03 18:17:27 +1200

    Update dit.rb

--- Formula/dit.rb
+++ Formula/dit.rb
@@ -2,7 +2,7 @@ class Dit < Formula
   desc "Developer Integrated Toolkit CLI"
   homepage "https://github.com/abnegate/dit"
   url "https://github.com/abnegate/dit/archive/refs/tags/0.4.0.tar.gz"
-  sha256 "6cb05bb5ede3d21dd12a386834e1c4f322238574c7bc8d946cf10d45d402b4b3"
+  sha256 "d170cd1fc46ce1cc430ffc9396ea6a12e3420d8c862ce907e6d21554800d9548"
   license "MIT"
 
   depends_on "docker" => :optional
