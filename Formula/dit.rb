1d644167d030a8e3dddb0d84d36e367cb149033e
Jake Barnby <jakeb994@gmail.com>
2022-08-03 18:16:44 +1200

    Update dit.rb

--- Formula/dit.rb
+++ Formula/dit.rb
@@ -1,7 +1,7 @@
 class Dit < Formula
   desc "Developer Integrated Toolkit CLI"
   homepage "https://github.com/abnegate/dit"
-  url "https://github.com/abnegate/dit/archive/refs/tags/0.3.0.tar.gz"
+  url "https://github.com/abnegate/dit/archive/refs/tags/0.4.0.tar.gz"
   sha256 "6cb05bb5ede3d21dd12a386834e1c4f322238574c7bc8d946cf10d45d402b4b3"
   license "MIT"
 
